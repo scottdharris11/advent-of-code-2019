5019e61bbb7eacf4ee0bf408bcb7f1e1027e82ce
scottdharris <scottdharris11@yahoo.com>
2024-12-14 05:52:16 -0500

    day 3, part 1

--- day03.py
+++ day03.py
@@ -5,20 +5,43 @@ from utilities.runner import runner
 @runner("Day 3", "Part 1")
 def solve_part1(lines: list[str]):
     """part 1 solving function"""
-    return 0
+    w1 = follow_wire(lines[0].split(","))
+    w2 = follow_wire(lines[1].split(","))
+    nearest = 0
+    for l in w2:
+        if l in w1:
+            dist = abs(l[0]) + abs(l[1])
+            if nearest == 0 or dist < nearest:
+                nearest = dist
+    return nearest
 
 @runner("Day 3", "Part 2")
 def solve_part2(lines: list[str]):
     """part 2 solving function"""
     return 0
 
+directions = {'R':(1,0), 'L':(-1,0), 'D':(0,1), 'U':(0,-1)}
+
+def follow_wire(instructions: list[str]) -> set[tuple[int,int]]:
+    """follow wire path to determine locations"""
+    locations = set()
+    loc = (0,0)
+    for i in instructions:
+        adjust = directions[i[0]]
+        steps = int(i[1:])
+        for _ in range(steps):
+            loc = (loc[0]+adjust[0], loc[1]+adjust[1])
+            locations.add(loc)
+    return locations
+
 # Data
 data = read_lines("input/day03/input.txt")
-sample = """""".splitlines()
+sample = """R8,U5,L5,D3
+U7,R6,D4,L4""".splitlines()
 
 # Part 1
-assert solve_part1(sample) == 0
-assert solve_part1(data) == 0
+assert solve_part1(sample) == 6
+assert solve_part1(data) == 1626
 
 # Part 2
 assert solve_part2(sample) == 0
