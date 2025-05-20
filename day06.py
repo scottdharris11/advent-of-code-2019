df34e2858175efd3f7184c8671ee3f6d971818e1
scottdharris <scottdharris11@yahoo.com>
2025-05-19 22:13:35 -0400

    day 6, part 2

--- day06.py
+++ day06.py
@@ -14,16 +14,28 @@ def solve_part1(lines: list[str]):
 @runner("Day 6", "Part 2")
 def solve_part2(lines: list[str]):
     """part 2 solving function"""
-    return 0
+    you = None
+    for o in parse_input(lines):
+        if o.name == "YOU":
+            you = o
+            break
+    return find_santa(you, set(), 0)
 
 class Object:
     """object orbit structure definition"""
     def __init__(self, name):
         self.name = name
-        self.orbits = []
+        self.orbits = set()
+        self.orbited_by = set()
 
     def __repr__(self):
         return self.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, value):
+        return self.name == value
 
     def orbit_count(self) -> int:
         """return the objects this object orbits"""
@@ -32,6 +44,22 @@ class Object:
             count += o.orbit_count()
         return count
 
+def find_santa(obj: Object, path: set, least: int) -> int:
+    """recursively search for SAN orbit"""
+    if "SAN" in obj.orbits or "SAN" in obj.orbited_by:
+        return len(path) - 1
+    if 0 < least < len(path) - 1:
+        return least
+    np = set(path)
+    np.add(obj.name)
+    for o in obj.orbits | obj.orbited_by:
+        if o.name in path:
+            continue
+        l = find_santa(o, np, least)
+        if least == 0 or l < least:
+            least = l
+    return least
+
 def parse_input(lines: list[str]) -> list[Object]:
     """parse input into objects"""
     objects = {}
@@ -39,8 +67,9 @@ def parse_input(lines: list[str]) -> list[Object]:
         o1, o2, *_ = line.split(")")
         obj1 = objects.setdefault(o1, Object(o1))
         obj2 = objects.setdefault(o2, Object(o2))
-        obj2.orbits.append(obj1)
-    return objects.values()
+        obj2.orbits.add(obj1)
+        obj1.orbited_by.add(obj2)
+    return list(objects.values())
 
 # Data
 data = read_lines("input/day06/input.txt")
@@ -55,11 +84,24 @@ D)I
 E)J
 J)K
 K)L""".splitlines()
+sample2 = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""".splitlines()
 
 # Part 1
 assert solve_part1(sample) == 42
 assert solve_part1(data) == 150150
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample2) == 4
+assert solve_part2(data) == 352
