5547e99c5441d88207728874b9276a5d9485e248
scottdharris <scottdharris11@yahoo.com>
2025-09-03 20:35:23 -0400

    day 13 part 1

--- day13.py
+++ day13.py
@@ -1,25 +1,131 @@
 """utility imports"""
-from utilities.data import read_lines
+from utilities.data import read_lines, parse_integers
 from utilities.runner import runner
 
 @runner("Day 13", "Part 1")
-def solve_part1(lines: list[str]):
+def solve_part1(line: str):
     """part 1 solving function"""
-    return 0
+    oc = parse_integers(line, ",")
+    counter = TileCounter()
+    computer = Computer(oc, counter)
+    computer.run()
+    return counter.tiles
 
 @runner("Day 13", "Part 2")
 def solve_part2(lines: list[str]):
     """part 2 solving function"""
     return 0
 
+class TileCounter:
+    """structure for painting robot"""
+    def __init__(self):
+        self.tiles = 0
+        self.count = 0
+
+    def accept_output(self, o: int):
+        """accept output value and perform robot actions accordingly"""
+        self.count += 1
+        if self.count % 3 == 0 and o == 2:
+            self.tiles += 1
+
+class Computer:
+    """structure for computer"""
+    def __init__(self, op: list[int], tc: TileCounter):
+        self.op = {i:o for i, o in enumerate(op)}
+        self.counter = tc
+        self.opi = 0
+        self.relative_base = 0
+        self.done = False
+
+    def run(self) -> bool:
+        """run the intcode program and return value"""
+        i = self.opi
+        while i < len(self.op):
+            opcode, m1, m2, m3 = self.opcode_modes(self.opc(i))
+            if opcode == 99:
+                self.done = True
+                break
+            if opcode == 1:
+                self.op[self.pvi(i+3,m3)] = self.pv(i+1,m1) + self.pv(i+2,m2)
+                i += 4
+            elif opcode == 2:
+                self.op[self.pvi(i+3,m3)] = self.pv(i+1,m1) * self.pv(i+2,m2)
+                i += 4
+            elif opcode == 3:
+                #self.op[self.pvi(i+1,m1)] = self.painter.provide_input()
+                i += 2
+            elif opcode == 4:
+                self.counter.accept_output(self.pv(i+1,m1))
+                i += 2
+            elif opcode == 5:
+                if self.pv(i+1,m1) != 0:
+                    i = self.pv(i+2,m2)
+                else:
+                    i += 3
+            elif opcode == 6:
+                if self.pv(i+1,m1) == 0:
+                    i = self.pv(i+2,m2)
+                else:
+                    i += 3
+            elif opcode == 7:
+                self.op[self.pvi(i+3,m3)] = 1 if self.pv(i+1,m1) < self.pv(i+2,m2) else 0
+                i += 4
+            elif opcode == 8:
+                self.op[self.pvi(i+3,m3)] = 1 if self.pv(i+1,m1) == self.pv(i+2,m2) else 0
+                i += 4
+            elif opcode == 9:
+                self.relative_base += self.pv(i+1,m1)
+                i += 2
+        self.opi = i
+
+    def opc(self, i: int) -> int:
+        """get the value of supplied index"""
+        return self.op.get(i, 0)
+
+    def opcode_modes(self, o: int) -> tuple[int,int,int,int]:
+        """parse opcode and modes from input"""
+        mode3 = 0
+        if o > 20000:
+            mode3 = 2
+            o -= 20000
+        elif o > 10000:
+            mode3 = 1
+            o -= 10000
+        mode2 = 0
+        if o > 2000:
+            mode2 = 2
+            o -= 2000
+        elif o > 1000:
+            mode2 = 1
+            o -= 1000
+        mode1 = 0
+        if o > 200:
+            mode1 = 2
+            o -= 200
+        elif o > 100:
+            mode1 = 1
+            o -= 100
+        return o, mode1, mode2, mode3
+
+    def pv(self, idx: int, mode: int) -> int:
+        """determine proper param value based on index and mode"""
+        if mode == 0:
+            return self.opc(self.opc(idx))
+        if mode == 1:
+            return self.opc(idx)
+        return self.opc(self.opc(idx) + self.relative_base)
+
+    def pvi(self, idx: int, mode: int) -> int:
+        """determine proper param value index based on index and mode"""
+        if mode in [0,1]:
+            return self.opc(idx)
+        return self.opc(idx) + self.relative_base
+
 # Data
-data = read_lines("input/day13/input.txt")
-sample = """""".splitlines()
+data = read_lines("input/day13/input.txt")[0]
 
 # Part 1
-assert solve_part1(sample) == 0
-assert solve_part1(data) == 0
+assert solve_part1(data) == 357
 
 # Part 2
-assert solve_part2(sample) == 0
 assert solve_part2(data) == 0
