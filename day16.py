2ad827ed39784da02254fe2043ef759038a419bb
scottdharris <scottdharris11@yahoo.com>
2025-09-12 08:15:00 -0400

    day 16 part 2, too slow, find repeating pattern?

--- day16.py
+++ day16.py
@@ -6,16 +6,13 @@ from utilities.runner import runner
 def solve_part1(line: str, phase_cnt: int) -> str:
     """part 1 solving function"""
     isignal = [int(ch) for ch in line]
-    osignal = [0] * len(isignal)
-    for _ in range(phase_cnt):
-        phase(isignal, osignal)
-        isignal, osignal = osignal, isignal
-    return "".join([str(i) for i in isignal[0:8]])
+    return signal_after_phase(isignal, phase_cnt)
 
 @runner("Day 16", "Part 2")
-def solve_part2(lines: list[str]) -> int:
+def solve_part2(line: str, phase_cnt: int) -> int:
     """part 2 solving function"""
-    return 0
+    isignal = [int(ch) for ch in line] * 10000
+    return signal_after_phase(isignal, phase_cnt)
 
 class RepeatPattern:
     """structure to enable applying repeating pattern"""
@@ -38,11 +35,22 @@ class RepeatPattern:
             self.seq_idx = 0
         return pattern
 
+def signal_after_phase(isignal: list[int], phase_cnt: int) -> str:
+    """compute the first 8 bytes of the output signal after applying number of phases"""
+    osignal = [0] * len(isignal)
+    for p in range(phase_cnt):
+        print(f"Phase {p+1} of {phase_cnt}")
+        phase(isignal, osignal)
+        isignal, osignal = osignal, isignal
+    return "".join([str(i) for i in isignal[0:8]])
+
 def phase(isignal: list[int], osignal: list[int]) -> None:
     """transform a signal within a phase"""
     slen = len(isignal)
     for idx in range(slen):
         osignal[idx] = phase_signal_position(isignal, idx+1, slen)
+        if idx % 1000 == 0:
+            print(f"Signal Index {idx+1} of {slen}, Output: {osignal[idx]}")
 
 def phase_signal_position(signal: list[int], position: int, signal_len: int) -> int:
     """calculate the next phase value for the supplied position"""
@@ -68,6 +76,9 @@ sample = """12345678""".splitlines()[0]
 sample2 = """80871224585914546619083218645595""".splitlines()[0]
 sample3 = """19617804207202209144916044189917""".splitlines()[0]
 sample4 = """69317163492948606335995924319873""".splitlines()[0]
+sample5 = """03036732577212944063491565474664""".splitlines()[0]
+sample6 = """02935109699940807407585447034323""".splitlines()[0]
+sample7 = """03081770884921959731165446850517""".splitlines()[0]
 
 # Part 1
 assert solve_part1(sample, 1) == "48226158"
@@ -80,5 +91,7 @@ assert solve_part1(sample4, 100) == "52432133"
 assert solve_part1(data, 100) == "11833188"
 
 # Part 2
-#assert solve_part2(sample) == 0
-#assert solve_part2(data) == 0
+assert solve_part2(sample5, 100) == "84462026"
+assert solve_part2(sample6, 100) == "78725270"
+assert solve_part2(sample7, 100) == "53553731"
+#assert solve_part2(data, 100) == ""
