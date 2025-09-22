1b3e346b2c89c6ad6245cace2a36f3ca26eb6065
scottdharris <scottdharris11@yahoo.com>
2025-09-22 13:58:07 -0400

    reversing functions and tests

--- day22.py
+++ day22.py
@@ -36,6 +36,19 @@ def shuffle_idx(instructions: list[str], size: int, idx: int) -> int:
             idx = cut_stack_idx(idx, point, size)
     return idx
 
+def rshuffle_idx(instructions: list[str], size: int, idx: int) -> int:
+    """reverse shuffle a deck of supplied size using instructions and follow index"""
+    for instruct in reversed(instructions):
+        if instruct == NEW_STACK_INSTRUCT:
+            idx = new_stack_idx(idx, size)
+        elif instruct.startswith(DEAL_INSTRUCT):
+            increment = int(instruct[len(DEAL_INSTRUCT):])
+            idx = rdeal_stack_idx(idx, increment, size)
+        elif instruct.startswith(CUT_INSTRUCT):
+            point = int(instruct[len(CUT_INSTRUCT):])
+            idx = cut_stack_idx(idx, point * -1, size)
+    return idx
+
 def new_stack_idx(idx: int, size: int) -> int:
     """index adjust based on new stack instruction (reverses stack)"""
     return size - idx - 1
@@ -80,6 +93,28 @@ assert cut_stack_idx(7, -4, 10) == 1
 assert cut_stack_idx(8, -4, 10) == 2
 assert cut_stack_idx(9, -4, 10) == 3
 
+# check reverse (original cut point * -1)
+assert cut_stack_idx(7, -3, 10) == 0
+assert cut_stack_idx(8, -3, 10) == 1
+assert cut_stack_idx(9, -3, 10) == 2
+assert cut_stack_idx(0, -3, 10) == 3
+assert cut_stack_idx(1, -3, 10) == 4
+assert cut_stack_idx(2, -3, 10) == 5
+assert cut_stack_idx(3, -3, 10) == 6
+assert cut_stack_idx(4, -3, 10) == 7
+assert cut_stack_idx(5, -3, 10) == 8
+assert cut_stack_idx(6, -3, 10) == 9
+assert cut_stack_idx(4, 4, 10) == 0
+assert cut_stack_idx(5, 4, 10) == 1
+assert cut_stack_idx(6, 4, 10) == 2
+assert cut_stack_idx(7, 4, 10) == 3
+assert cut_stack_idx(8, 4, 10) == 4
+assert cut_stack_idx(9, 4, 10) == 5
+assert cut_stack_idx(0, 4, 10) == 6
+assert cut_stack_idx(1, 4, 10) == 7
+assert cut_stack_idx(2, 4, 10) == 8
+assert cut_stack_idx(3, 4, 10) == 9
+
 def deal_stack_idx(idx: int, increment: int, size: int) -> int:
     """index adjust based on deal increment"""
     if idx == 0:
@@ -112,6 +147,39 @@ assert deal_stack_idx(6, 3, 10) == 8
 assert deal_stack_idx(7, 3, 10) == 1
 assert deal_stack_idx(8, 3, 10) == 4
 assert deal_stack_idx(9, 3, 10) == 7
+
+def rdeal_stack_idx(idx: int, increment: int, size: int) -> int:
+    """reverse index adjust based on deal increment"""
+    if idx == 0:
+        return idx
+    pass_remain = size % increment
+    pass_increment = (size // increment) + 1
+    if idx % increment == 0:
+        return idx // increment
+    pidx = pass_increment
+    nidx = (pass_increment * increment) - size
+    while nidx != idx:
+        pincr = pass_increment
+        if nidx > pass_remain:
+            pincr -= 1
+        if nidx < idx and (idx - nidx) % increment == 0:
+            pidx += (idx - nidx) // increment
+            nidx = idx
+        else:
+            nidx = nidx + (pincr * increment) - size
+            pidx += pincr
+    return pidx
+
+assert rdeal_stack_idx(0, 3, 10) == 0
+assert rdeal_stack_idx(3, 3, 10) == 1
+assert rdeal_stack_idx(6, 3, 10) == 2
+assert rdeal_stack_idx(9, 3, 10) == 3
+assert rdeal_stack_idx(2, 3, 10) == 4
+assert rdeal_stack_idx(5, 3, 10) == 5
+assert rdeal_stack_idx(8, 3, 10) == 6
+assert rdeal_stack_idx(1, 3, 10) == 7
+assert rdeal_stack_idx(4, 3, 10) == 8
+assert rdeal_stack_idx(7, 3, 10) == 9
 
 # Data
 data = read_lines("input/day22/input.txt")
@@ -183,4 +251,50 @@ assert shuffle_idx(sample4, 10, 9) == 0
 assert solve_part1(data) == 8502
 
 # Part 2
+assert rshuffle_idx(sample, 10, 0) == 0
+assert rshuffle_idx(sample, 10, 7) == 1
+assert rshuffle_idx(sample, 10, 4) == 2
+assert rshuffle_idx(sample, 10, 1) == 3
+assert rshuffle_idx(sample, 10, 8) == 4
+assert rshuffle_idx(sample, 10, 5) == 5
+assert rshuffle_idx(sample, 10, 2) == 6
+assert rshuffle_idx(sample, 10, 9) == 7
+assert rshuffle_idx(sample, 10, 6) == 8
+assert rshuffle_idx(sample, 10, 3) == 9
+
+assert rshuffle_idx(sample2, 10, 1) == 0
+assert rshuffle_idx(sample2, 10, 4) == 1
+assert rshuffle_idx(sample2, 10, 7) == 2
+assert rshuffle_idx(sample2, 10, 0) == 3
+assert rshuffle_idx(sample2, 10, 3) == 4
+assert rshuffle_idx(sample2, 10, 6) == 5
+assert rshuffle_idx(sample2, 10, 9) == 6
+assert rshuffle_idx(sample2, 10, 2) == 7
+assert rshuffle_idx(sample2, 10, 5) == 8
+assert rshuffle_idx(sample2, 10, 8) == 9
+
+assert rshuffle_idx(sample3, 10, 2) == 0
+assert rshuffle_idx(sample3, 10, 5) == 1
+assert rshuffle_idx(sample3, 10, 8) == 2
+assert rshuffle_idx(sample3, 10, 1) == 3
+assert rshuffle_idx(sample3, 10, 4) == 4
+assert rshuffle_idx(sample3, 10, 7) == 5
+assert rshuffle_idx(sample3, 10, 0) == 6
+assert rshuffle_idx(sample3, 10, 3) == 7
+assert rshuffle_idx(sample3, 10, 6) == 8
+assert rshuffle_idx(sample3, 10, 9) == 9
+
+assert rshuffle_idx(sample4, 10, 7) == 0
+assert rshuffle_idx(sample4, 10, 4) == 1
+assert rshuffle_idx(sample4, 10, 1) == 2
+assert rshuffle_idx(sample4, 10, 8) == 3
+assert rshuffle_idx(sample4, 10, 5) == 4
+assert rshuffle_idx(sample4, 10, 2) == 5
+assert rshuffle_idx(sample4, 10, 9) == 6
+assert rshuffle_idx(sample4, 10, 6) == 7
+assert rshuffle_idx(sample4, 10, 3) == 8
+assert rshuffle_idx(sample4, 10, 0) == 9
+
+assert rshuffle_idx(data, 10007, 8502) == 2019
+
 assert solve_part2(data) == 0
