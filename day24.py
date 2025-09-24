144dd044199422a5cc582067d76dd282bef8f667
scottdharris <scottdharris11@yahoo.com>
2025-09-24 09:01:17 -0400

    day 24, part 2 sample off

--- day24.py
+++ day24.py
@@ -5,7 +5,7 @@ from utilities.runner import runner
 @runner("Day 24", "Part 1")
 def solve_part1(lines: list[str]):
     """part 1 solving function"""
-    bugs = parse_input(lines)
+    bugs = parse_input(lines, False)
     previous = set()
     previous.add(frozenset(bugs))
     while True:
@@ -17,9 +17,26 @@ def solve_part1(lines: list[str]):
     return biodiversity(bugs)
 
 @runner("Day 24", "Part 2")
-def solve_part2(lines: list[str]):
+def solve_part2(lines: list[str], minutes: int):
     """part 2 solving function"""
-    return 0
+    bugs = parse_input(lines, True)
+    min_depth = 0
+    max_depth = 0
+    for m in range(minutes):
+        bugs, min_depth, max_depth = rnext_bug_state(bugs, min_depth, max_depth)
+        print(f"after minute {m+1}: min_depth = {min_depth}, max_depth = {max_depth}")
+        for d in range(min_depth, max_depth+1):
+            print(f"Depth {d}")
+            for y in range(5):
+                line = ""
+                for x in range(5):
+                    if (d,x,y) in bugs:
+                        line += '#'
+                    else:
+                        line += '.'
+                print(line)
+            print("")
+    return len(bugs)
 
 def biodiversity(bugs: set[tuple[int,int]]) -> int:
     """calcluate the biodiversity of the bug state"""
@@ -31,7 +48,7 @@ def biodiversity(bugs: set[tuple[int,int]]) -> int:
                 rating += pow(2, p)
     return rating
 
-def next_bug_state(bugs: set[tuple[int,int]]) -> set[tuple[int,int]]:
+def next_bug_state(bugs: set[tuple[int,int,int]]) -> set[tuple[int,int]]:
     """determine next bug state from current"""
     nbugs = set()
     for y in range(5):
@@ -48,17 +65,81 @@ def next_bug_state(bugs: set[tuple[int,int]]) -> set[tuple[int,int]]:
                 if adjancent == 1:
                     nbugs.add((x,y))
             else:
-                if adjancent == 1 or adjancent == 2:
+                if adjancent in [1,2]:
                     nbugs.add((x,y))
     return nbugs
 
-def parse_input(lines: list[str]) -> set[tuple[int,int]]:
+RMOVES = {
+    (0,0): [(-1,2,1),(0,1,0),(0,0,1),(-1,1,2)], #1!
+    (1,0): [(-1,2,1),(0,2,0),(0,1,1),(0,0,0)], #2!
+    (2,0): [(-1,2,1),(0,3,0),(0,2,1),(0,1,0)], #3!
+    (3,0): [(-1,2,1),(0,4,0),(0,3,1),(0,2,0)], #4!
+    (4,0): [(-1,2,1),(-1,3,2),(0,4,1),(0,3,0)], #5!
+
+    (0,1): [(0,0,0),(0,1,1),(0,0,2),(-1,1,2)], #6!
+    (1,1): [(0,1,0),(0,2,1),(0,1,3),(0,0,1)], #7!
+    (2,1): [(0,2,0),(0,3,1),(1,0,0),(1,1,0),(1,2,0),(1,3,0),(1,4,0),(0,1,1)], #8!
+    (3,1): [(0,3,0),(0,4,1),(0,3,2),(0,2,1)], #9!
+    (4,1): [(0,4,0),(-1,3,2),(0,4,2),(0,3,1)], #10!
+
+    (0,2): [(0,0,1),(0,1,2),(0,0,3),(-1,1,2)], #11!
+    (1,2): [(0,1,1),(1,0,0),(1,0,1),(1,0,2),(1,0,3),(1,0,4),(0,1,3),(0,0,2)], #12!
+    (2,2): None, #? (Middle: portal to next depth)
+    (3,2): [(0,3,1),(0,4,2),(0,3,3),(1,4,0),(1,4,1),(1,4,2),(1,4,3),(1,4,4)], #14!
+    (4,2): [(0,4,1),(-1,3,2),(0,4,3),(0,3,2)], #15!
+
+    (0,3): [(0,0,2),(0,1,3),(0,0,4),(-1,1,2)], #16!
+    (1,3): [(0,1,2),(0,2,3),(0,1,4),(0,0,3)], #17!
+    (2,3): [(1,0,4),(1,1,4),(1,2,4),(1,3,4),(1,4,4),(0,3,3),(0,2,4),(0,1,3)], #18!
+    (3,3): [(0,3,2),(0,4,3),(0,3,4),(0,2,3)], #19!
+    (4,3): [(0,4,2),(-1,3,2),(0,4,4),(0,3,3)],  #20!
+
+    (0,4): [(0,0,3),(0,1,4),(-1,2,3),(-1,1,2)], #21!
+    (1,4): [(0,1,3),(0,2,4),(-1,2,3),(0,0,4)], #22!
+    (2,4): [(0,2,3),(0,3,4),(-1,2,3),(0,1,4)], #23!
+    (3,4): [(0,3,3),(0,4,4),(-1,2,3),(0,2,4)], #24!
+    (4,4): [(0,4,3),(-1,3,2),(-1,2,3),(0,3,4)], #25!
+}
+
+def rnext_bug_state(bugs: set[tuple[int,int,int]], min_depth: int, max_depth: int):
+    """determine next bug state from current"""
+    nbugs = set()
+    nmin_depth = min_depth
+    nmax_depth = max_depth
+    for d in range(min_depth-1, max_depth+2):
+        for y in range(5):
+            for x in range(5):
+                if y == 2 and x == 2:
+                    continue
+                adjancent = 0
+                for moves in RMOVES[(x,y)]:
+                    ad = d + moves[0]
+                    ax = moves[1]
+                    ay = moves[2]
+                    if (ad,ax,ay) in bugs:
+                        adjancent += 1
+                if (d,x,y) in bugs:
+                    if adjancent == 1:
+                        nbugs.add((d,x,y))
+                        nmin_depth = min(d,nmin_depth)
+                        nmax_depth = max(d,nmax_depth)
+                else:
+                    if 0 < adjancent < 3:
+                        nbugs.add((d,x,y))
+                        nmin_depth = min(d,nmin_depth)
+                        nmax_depth = max(d,nmax_depth)
+    return nbugs, nmin_depth, nmax_depth
+
+def parse_input(lines: list[str], recursive: bool) -> set[tuple[int,int,int]]:
     """parse input for problem"""
     bugs = set()
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
             if c == '#':
-                bugs.add((x,y))
+                if recursive:
+                    bugs.add((0,x,y))
+                else:
+                    bugs.add((x,y))
     return bugs
 
 # Data
@@ -74,5 +155,5 @@ assert solve_part1(sample) == 2129920
 assert solve_part1(data) == 18371095
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample, 10) == 99
+assert solve_part2(data, 200) == 0
