9a8ded3b8dbd64322bc42c16ade2a4ff1d864e68
scottdharris <scottdharris11@yahoo.com>
2025-10-01 18:38:49 -0400

    day 16, part 2

--- day16.py
+++ day16.py
@@ -12,7 +12,23 @@ def solve_part1(line: str, phase_cnt: int) -> str:
 def solve_part2(line: str, phase_cnt: int) -> int:
     """part 2 solving function"""
     isignal = [int(ch) for ch in line] * 10000
-    return signal_after_phase(isignal, phase_cnt)
+    # took some hints to look at the output from the first part
+    # after each phase and it became apparent that the back half
+    # of the numbers always represented a sum values from the back mod 10.
+    # given that the offset, put the values we cared about in the
+    # back half, don't need to worry about the pattern logic in the
+    # first half, so just focused on processing the pieces after the
+    # offset value in reverse for each phase transformation.
+    offset = int(line[0:7])
+    isignal = isignal[offset:]
+    osignal = [0] * len(isignal)
+    for _ in range(phase_cnt):
+        total = 0
+        for idx in range(len(isignal)-1,-1,-1):
+            total += isignal[idx]
+            osignal[idx] = total % 10
+        isignal, osignal = osignal, isignal
+    return "".join([str(i) for i in isignal[0:8]])
 
 class RepeatPattern:
     """structure to enable applying repeating pattern"""
@@ -38,8 +54,7 @@ class RepeatPattern:
 def signal_after_phase(isignal: list[int], phase_cnt: int) -> str:
     """compute the first 8 bytes of the output signal after applying number of phases"""
     osignal = [0] * len(isignal)
-    for p in range(phase_cnt):
-        print(f"Phase {p+1} of {phase_cnt}")
+    for _ in range(phase_cnt):
         phase(isignal, osignal)
         isignal, osignal = osignal, isignal
     return "".join([str(i) for i in isignal[0:8]])
@@ -49,8 +64,6 @@ def phase(isignal: list[int], osignal: list[int]) -> None:
     slen = len(isignal)
     for idx in range(slen):
         osignal[idx] = phase_signal_position(isignal, idx+1, slen)
-        if idx % 1000 == 0:
-            print(f"Signal Index {idx+1} of {slen}, Output: {osignal[idx]}")
 
 def phase_signal_position(signal: list[int], position: int, signal_len: int) -> int:
     """calculate the next phase value for the supplied position"""
@@ -94,4 +107,4 @@ assert solve_part1(data, 100) == "11833188"
 assert solve_part2(sample5, 100) == "84462026"
 assert solve_part2(sample6, 100) == "78725270"
 assert solve_part2(sample7, 100) == "53553731"
-#assert solve_part2(data, 100) == ""
+assert solve_part2(data, 100) == "55005000"
